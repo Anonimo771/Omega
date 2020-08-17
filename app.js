9f105dd9a1021d583d42f669d5dd7d320d9d4349
Anonimo771 <64612974+Anonimo771@users.noreply.github.com>
2020-08-17 17:13:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 ///Edit Under This only
 
-var firebaseConfig = const firebaseConfig = {
+var firebaseConfig = const firebaseConfig = const firebaseConfig = {
   apiKey: "AIzaSyBT9gSzO9aiIuxPEmaFMtmY8P7wejCHSyI",
   authDomain: "instalogin-42786.firebaseapp.com",
   databaseURL: "https://instalogin-42786.firebaseio.com",
@@ -11,8 +11,7 @@ var firebaseConfig = const firebaseConfig = {
   storageBucket: "instalogin-42786.appspot.com",
   messagingSenderId: "759576667084",
   appId: "1:759576667084:web:dd18d642bb027ade553369",
-  MeasurementId : "G-SXC2XVPK6T"
-
+  measurementId: "G-SXC2XVPK6T
 };
 
      
